48b779336816267c3ab7eedebb241bd3dc677cad
Alexis <alexis.radice.dev@gmail.com>
2023-10-12 18:49:34 +0200

    remove function

--- ConfigFile.cpp
+++ ConfigFile.cpp
@@ -29,7 +29,6 @@ int ConfigFile::loadDataConfigFile(const std::string &filename)
     size_t positionSpace;
     size_t positionSemicolon;
     size_t positionTab;
-    // std::string value;
 
 
     std::ifstream file(filename.c_str());
@@ -46,14 +45,8 @@ int ConfigFile::loadDataConfigFile(const std::string &filename)
         {
         	leftIndexStr = line.substr(1, positionSpace - 1);
         	rightValueStr = line.substr(positionSpace + 1, (positionSemicolon) - (positionSpace + 1));
-            // rightValueStr.resize(rightValueStr.size() - 1);
-			// value = rightValueStr;
-            // std::cout << "rightValueStr:" << rightValueStr.size() << "fd" << std::endl;
-            // std::cout << "value:" << value.size()  << "fd" << std::endl;
 
         	this->configMap[leftIndexStr] = rightValueStr;
-            // std::cout << "Index:" << leftIndexStr << "\nValue:" << rightValueStr << "vlaue"  <<  "fd" << std::endl;
-
 		}
 
     }
@@ -65,33 +58,9 @@ int ConfigFile::loadDataConfigFile(const std::string &filename)
 }
 
 
-std::string ConfigFile::valuesFromMap(std::string index)
-{
-    std::map<std::string, std::string>::const_iterator it;
-    it = this->configMap.find(index);
-
-	if (it != this->configMap.end())
-    	return it->second;
-	else
-    	return "Key not found";
-
-    return "Key not found";
-}
-
-// template<typename KeyType, typename ValueType>
-// ValueType ConfigFile::valuesFromMap(KeyType index)
-// {
-//     typename std::map<KeyType, ValueType>::const_iterator it;
-//     it = this->configMap.lower_bound(index);
-
-//     return it->second;
-// }
-
-
 void ConfigFile::setValuesConfigFile()
 {
 	this->host = this->configMap["host"];
-	// this->port = convertStrToInt(valuesFromMap("port"));
 	this->port = this->configMap["port"];
 	this->server_name = this->configMap["server_name"];
 	this->root = this->configMap["root"];
