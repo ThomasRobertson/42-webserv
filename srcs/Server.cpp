6773480b12479a80e53b44af28621382bcb17280
Romeo VINCENT <rvincent@made-f0Br7s18.clusters.42paris.fr>
2023-10-17 17:43:23 +0200

    merge

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -10,23 +10,8 @@ Server::~Server()
     return ;
 }
 
-void listenClientRequest(int serverSocket)
+void Server::listenClientRequest(int serverSocket)
 {
-    std::ifstream htmlFile("www/srcs/index.html"); 
-    if (!htmlFile.is_open())
-    {
-        std::cerr << "Failed to open HTML file." << std::endl;
-        return ;
-    }
-
-    std::string htmlContent((std::istreambuf_iterator<char>(htmlFile)), std::istreambuf_iterator<char>());
-
-    std::string response = "HTTP/1.1 200 OK\r\n";
-    response += "Content-Type: text/html\r\n";
-    response += "Content-Length: " + htmlContent + "\r\n";
-    response += "\r\n";
-    response += htmlContent;
-
     while (true)
     {
         sockaddr_in clientAddress;
@@ -61,7 +46,6 @@ void listenClientRequest(int serverSocket)
     }
 }
 
-        
 int Server::startServer()
 {
     ConfigFile configFile = this->_configFile;
@@ -78,7 +62,46 @@ int Server::startServer()
         std::cerr << "Error retrieving host information" << std::endl;
         return 0;
     }
-    
+
+    int serverSocket;
+
+    for (p = serverInfo; p != NULL; p = p->ai_next)
+    {
+        serverSocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (serverSocket == -1)
+            continue;
+
+        int iSetOption = 1;
+        setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&iSetOption, sizeof(iSetOption));
+
+        if (bind(serverSocket, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(serverSocket);
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(serverInfo);
+
+    if (p == NULL)
+    {
+        std::cerr << "Socket binding error" << std::endl;
+        return 0;
+    }
+
+    if (listen(serverSocket, 5) == -1)
+    {
+        std::cerr << "Socket listening error" << std::endl;
+        close(serverSocket);
+        return 0;
+    }
+
+    std::cout << "Server listening on port " << port << "..." << std::endl << std::endl;
+    listenClientRequest(serverSocket);
+
     close(serverSocket);
-    return 0;
+    std::cout << "SERVER CLOSED\n";
+    return 1;
 }
