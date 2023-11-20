144726ec368eccbfb1fe30d1df03439d8d209539
ThomasRobertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-11-20 17:30:31 +0000

    update cgi-test

--- cgi-bin/test-cgi.c
+++ cgi-bin/test-cgi.c
@@ -1,46 +1,55 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 4096
 
+void putstr(char *str)
+{
+	int len = 0;
+	while (str[len])
+		len++;
+	write(STDOUT_FILENO, str, len);
+	write(STDOUT_FILENO, "\n", 1);
+}
+
 int main(int ac, char **av, char **env)
 {
-	(void)ac;                                                                               
-	printf("       444444444   222222222222222                 CCCCCCCCCCCCC       GGGGGGGGGGGGGIIIIIIIIII\n"),
-	printf("      4::::::::4  2:::::::::::::::22            CCC::::::::::::C    GGG::::::::::::GI::::::::I\n"),
-	printf("     4:::::::::4  2::::::222222:::::2         CC:::::::::::::::C  GG:::::::::::::::GI::::::::I\n"),
-	printf("    4::::44::::4  2222222     2:::::2        C:::::CCCCCCCC::::C G:::::GGGGGGGG::::GII::::::II\n"),
-	printf("   4::::4 4::::4              2:::::2       C:::::C       CCCCCCG:::::G       GGGGGG  I::::I  \n"),
-	printf("  4::::4  4::::4              2:::::2      C:::::C             G:::::G                I::::I  \n"),
-	printf(" 4::::4   4::::4           2222::::2       C:::::C             G:::::G                I::::I  \n"),
-	printf("4::::444444::::444    22222::::::22        C:::::C             G:::::G    GGGGGGGGGG  I::::I  \n"),
-	printf("4::::::::::::::::4  22::::::::222          C:::::C             G:::::G    G::::::::G  I::::I  \n"),
-	printf("4444444444:::::444 2:::::22222             C:::::C             G:::::G    GGGGG::::G  I::::I  \n"),
-	printf("          4::::4  2:::::2                  C:::::C             G:::::G        G::::G  I::::I  \n"),
-	printf("          4::::4  2:::::2                   C:::::C       CCCCCCG:::::G       G::::G  I::::I  \n"),
-	printf("          4::::4  2:::::2       222222       C:::::CCCCCCCC::::C G:::::GGGGGGGG::::GII::::::II\n"),
-	printf("        44::::::442::::::2222222:::::2        CC:::::::::::::::C  GG:::::::::::::::GI::::::::I\n"),
-	printf("        4::::::::42::::::::::::::::::2          CCC::::::::::::C    GGG::::::GGG:::GI::::::::I\n"),
-	printf("        444444444422222222222222222222             CCCCCCCCCCCCC       GGGGGG   GGGGIIIIIIIIII\n"),
+	(void)ac;
+	
+	putstr("       444444444   222222222222222                 CCCCCCCCCCCCC       GGGGGGGGGGGGGIIIIIIIIII"),
+	putstr("      4::::::::4  2:::::::::::::::22            CCC::::::::::::C    GGG::::::::::::GI::::::::I"),
+	putstr("     4:::::::::4  2::::::222222:::::2         CC:::::::::::::::C  GG:::::::::::::::GI::::::::I"),
+	putstr("    4::::44::::4  2222222     2:::::2        C:::::CCCCCCCC::::C G:::::GGGGGGGG::::GII::::::II"),
+	putstr("   4::::4 4::::4              2:::::2       C:::::C       CCCCCCG:::::G       GGGGGG  I::::I  "),
+	putstr("  4::::4  4::::4              2:::::2      C:::::C             G:::::G                I::::I  "),
+	putstr(" 4::::4   4::::4           2222::::2       C:::::C             G:::::G                I::::I  "),
+	putstr("4::::444444::::444    22222::::::22        C:::::C             G:::::G    GGGGGGGGGG  I::::I  "),
+	putstr("4::::::::::::::::4  22::::::::222          C:::::C             G:::::G    G::::::::G  I::::I  "),
+	putstr("4444444444:::::444 2:::::22222             C:::::C             G:::::G    GGGGG::::G  I::::I  "),
+	putstr("          4::::4  2:::::2                  C:::::C             G:::::G        G::::G  I::::I  "),
+	putstr("          4::::4  2:::::2                   C:::::C       CCCCCCG:::::G       G::::G  I::::I  "),
+	putstr("          4::::4  2:::::2       222222       C:::::CCCCCCCC::::C G:::::GGGGGGGG::::GII::::::II"),
+	putstr("        44::::::442::::::2222222:::::2        CC:::::::::::::::C  GG:::::::::::::::GI::::::::I"),
+	putstr("        4::::::::42::::::::::::::::::2          CCC::::::::::::C    GGG::::::GGG:::GI::::::::I"),
+	putstr("        444444444422222222222222222222             CCCCCCCCCCCCC       GGGGGG   GGGGIIIIIIIIII"),
 
-	printf("\n\nDisplaying arguments :\n");
+	putstr("\n\nDisplaying arguments :");
 	for (int i = 0; av[i]; i++)
-		printf("%s\n", av[i]);
-	printf("\n\nDisplaying env :\n");
+		putstr(av[i]);
+	putstr("\n\nDisplaying env :");
 	for (int i = 0; env[i]; i++)
-		printf("%s\n", env[i]);
+		putstr(env[i]);
 
-	printf("\n\nDisplaying content of file :\n");
-	FILE* file = fopen(av[1], "r");
-	char buffer[BUFFER_SIZE];
-	if (file == NULL)
+	putstr("\n\nDisplaying content of file is :");
+	char ch[BUFFER_SIZE];
+	size_t len = 0;
+	do
 	{
-		printf("Could not read the file : %s\n", strerror(errno));
-		return 0;
-	}
-	int read = 0;
-	while (read = fread(&buffer, BUFFER_SIZE, sizeof(char), file), read) {
-		printf("%.*s", BUFFER_SIZE, buffer);
-	}
+		len = read(STDIN_FILENO, ch, BUFFER_SIZE);
+		//putstr(ch, len);
+	} while (len < 0);
+
+	putstr("\n\nEND OF FILE\n");
 }
