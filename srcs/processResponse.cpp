e6450f2dd7d74a55f28c5109f8321f46a4156929
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-11-23 18:36:30 +0100

    generate error 500 if exeption catch when generating response

--- srcs/processResponse.cpp
+++ srcs/processResponse.cpp
@@ -30,27 +30,34 @@ void StartServers::processResponse(epoll_event currentEvent)
 
 	std::cout << "----------------------- NEW REPONSE: " << currentEvent.data.fd << " -----------------------" << std::endl;
 
-	if (isCGIFile(currentServer, currentClient.request.root))
+	try
 	{
-		response = genMethod.CGIMethod();
+		if (isCGIFile(currentServer, currentClient.request.root))
+		{
+			response = genMethod.CGIMethod();
+		}
+		else if (currentClient.request.method == "GET")
+		{
+			response = genMethod.GETMethod();
+		}
+		else if (currentClient.request.method == "POST")
+		{
+			// std::cout << currentClient.request.body << std::endl;
+			response = genMethod.POSTMethod();
+		}
+		else if (currentClient.request.method == "DELETE")
+		{
+			// std::cout << "DELETE METH" << std::endl;
+			response = genMethod.DELETEMethod();
+		}
+		else
+		{
+			response = genMethod.getErrorPageResponse("405");
+		}		
 	}
-	else if (currentClient.request.method == "GET")
+	catch (const std::exception&)
 	{
-		response = genMethod.GETMethod();
-	}
-	else if (currentClient.request.method == "POST")
-	{
-		// std::cout << currentClient.request.body << std::endl;
-		response = genMethod.POSTMethod();
-	}
-	else if (currentClient.request.method == "DELETE")
-	{
-		// std::cout << "DELETE METH" << std::endl;
-		response = genMethod.DELETEMethod();
-	}
-	else
-	{
-		response = genMethod.getErrorPageResponse("405");
+		response = genMethod.getErrorPageResponse("500");
 	}
 
 	// if (isValidRequest(client.request))
